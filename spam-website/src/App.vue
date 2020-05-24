f3e6cd054d9d22a7d4b4d423665d4f7775f173c7
tipie101 <timka.timkah@gmail.com>
2020-05-24 13:59:39 +0200

    quick and dirty website set up

---
+++ spam-website/src/App.vue
@@ -0,0 +1,41 @@
+<!-- TODO -->
+<!-- SoundCloud Player -->
+<!-- https://codepen.io/notoriousb1t/pen/BNPLRo -->
+<!-- https://fsymbols.com/generators/tarty/ -->
+
+<template>
+<div>
+  <div id="app">
+    <img src="./assets/spam2.png" />
+    <router-view/> 
+  </div>
+
+  <!-- colors: %2334495e (graublau), %2341b883 (mint) -->
+  <iframe class="footer" width="100%" height="100" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/443532657&color=%2341b883&auto_play=false&hide_related=false&show_comments=false&show_user=false&show_reposts=false&show_teaser=false&visual=false">
+  </iframe>
+
+</div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  /* color: #2c3e50; */
+  
+  margin-top: 60px;
+}
+.footer {
+  position:fixed;
+  bottom:0;
+}
+
+</style>
---
+++ spam-website/src/router/index.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '@/components/Home'
+import Events from '@/components/Events'
+import Info from '@/components/Info'
+import Upcoming from '@/components/Upcoming'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/events',
+      name: 'Events',
+      component: Events
+    },
+    {
+      path: '/info',
+      name: 'Info',
+      component: Info
+    },
+    {
+      path: '/upcoming',
+      name: 'Upcoming',
+      component: Upcoming
+    },
+  ]
+})
