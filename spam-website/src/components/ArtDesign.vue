87a716106f186d14b16ec7af30c26cb1cd9455de
tipie101 <timka.timkah@gmail.com>
2020-08-03 17:20:11 +0200

    layout for poster-grid with some css-fxs

--- spam-website/src/components/ArtDesign.vue
+++ spam-website/src/components/ArtDesign.vue
@@ -3,9 +3,69 @@
 <template>
   <div class="container">
     <navigation activeTab="art & design"></navigation>
-    <button id="color" class="animated" @click="turnRed">Click me</button>
+    <!-- <button id="color" class="animated" @click="turnRed">Click me</button>
     <div id="animation">
-      <!-- animation with THREE.js -->
+      TODO: animation with THREE.js
+    </div> -->
+    <div class="foto-grid">
+      <!-- create grid with posters -->
+      <div class="row">
+        <div class="col-md-6">
+          <div class="flip-box">
+            <div class="flip-box-inner">
+              <div class="flip-box-back">
+                <img src="src/assets/spam_party_posters/space_spam.jpg" class="img-fluid" alt="Responsive image">
+              </div>
+              <div class="flip-box-front">
+                <div class="flip-box-text">
+                  <br>
+                  <h2>SPAM PARTY EVENTS</h2>
+                  <p>What an amazing stuff</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <img src="src/assets/spam_party_posters/scify_poster.jpg" class="img-fluid" alt="Responsive image">
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-4">
+          <img src="src/assets/spam_party_posters/baby_spam.jpg" class="img-fluid" alt="Responsive image">
+          <img src="src/assets/spam_party_posters/jungle_spam_original.jpg" class="img-fluid" alt="Responsive image">
+        </div>
+        <div class="col-md-4">
+          <img src="src/assets/spam_party_posters/2nd_aniversary_vertical.jpg" class="img-fluid" alt="Responsive image">
+        </div>
+        <div class="col-md-4">
+          <img src="src/assets/spam_party_posters/fiktiva.jpg" class="img-fluid" alt="Responsive image">
+         
+          <!-- TODO: Make images same size -->
+          <div class="flip-box">
+            <div class="flip-box-inner">
+              <div class="flip-box-back">
+                <img src="src/assets/spam_party_posters/el_dorado_faces.jpg" class="img-fluid" alt="Responsive image">
+              </div>
+              <div class="flip-box-front">
+                <img src="src/assets/spam_party_posters/banner_dorado.jpg" class="img-fluid" alt="Responsive image">
+              </div>
+            </div>
+          </div>
+          
+        </div>
+      </div>   
+      <div class="row">
+        <div class="col-md-4">
+          <img src="src/assets/spam_party_posters/spam_no7_poster.jpg" class="img-fluid" alt="Responsive image">
+        </div>
+        <div class="col-md-4">
+          <img src="src/assets/spam_party_posters/spams_funeral.jpg" class="img-fluid" alt="Responsive image">
+        </div>
+        <div class="col-md-4">
+          <img src="src/assets/spam_party_posters/tax_scam_spam.jpg" class="img-fluid" alt="Responsive image">
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -74,4 +134,63 @@ export default {
   transition-duration: 3s;
   margin: 300px auto;
 }
+
+.foto-grid {
+  margin: 300px 0px 500px 0px;
+}
+
+.foto-grid img {
+  margin: 10px;
+  padding: 0px;
+}
+
+/* The flip box container - set the width and height to whatever you want. We have added the border property to demonstrate that the flip itself goes out of the box on hover (remove perspective if you don't want the 3D effect */
+.flip-box {
+  background-color: transparent;
+  perspective: 1000px; /* Remove this if you don't want the 3D effect */
+  /* width: 200px; */
+  /* height: 200px; */
+}
+
+/* This container is needed to position the front and back side */
+.flip-box-inner {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  text-align: center; 
+  transition: transform 0.8s;
+  transform-style: preserve-3d;
+}
+
+/* Do an horizontal flip when you move the mouse over the flip box container */
+.flip-box:hover .flip-box-inner {
+  transform: rotateY(180deg);
+}
+
+.flip-box:hover .flip-box-text {
+  opacity: 0.01;
+  transition-property: opacity;
+  transition-duration: 1s;
+}
+
+/* Position the front and back side */
+.flip-box-front, .flip-box-back {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  -webkit-backface-visibility: hidden; /* Safari */
+  backface-visibility: hidden;
+}
+
+/* Style the front side (fallback if image is missing) */
+.flip-box-front {
+  background-color: white;
+  color: #34495E;
+}
+
+/* Style the back side */
+.flip-box-back {
+  transform: rotateY(180deg);
+}
+
 </style>
